de613463d7f11d2daa94918ca97f6a13ea7932e2
Sam Newell <samnewell91@gmail.com>
2024-10-16 13:49:20 +0100

    Can't get styles to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@ const { Metal_Mania } = require("next/font/google");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    "./pages/**/*.{js, ts, jsx, tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
