dcc8aae888df01f01e6d58c2ccc02a234668fb51
Sam Newell <samnewell91@gmail.com>
2024-10-16 13:20:13 +0100

    debugging for deployment

--- Components/Menu.js
+++ Components/Menu.js
@@ -13,7 +13,7 @@ export default function Menu({ openMenu, setOpenMenu }) {
 
   return (
     <div
-      className={`menu flex flex-col fixed items-center inset-0 justify-between  h-full w-full bg-yellow-400 transition-all ${
+      className={`menu hidden flex flex-col fixed items-center inset-0 justify-between  h-full w-full bg-yellow-400 transition-all ${
         openMenu ? "translate-x-0" : "-translate-x-full"
       }`}
     >
